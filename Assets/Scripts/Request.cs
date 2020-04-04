d9250883cb47df8ad35885e8581b7de11f8767ba
Vgeorgee <cracken1111@gmail.com>
2020-04-04 21:35:37 +0200

    asda

---
+++ Assets/Scripts/Request.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+public class Request : MonoBehaviour
+{
+
+    public static bool local = true;
+
+    public static string domain = "http://localhost";
+    public static string api = "/api";
+
+    public InputField usernameField;
+    public InputField passwordField;
+
+    public static string login = domain + api + "/login";
+    public static string leaderboard = domain + api + "/leaderboard";
+
+
+    void Start() {
+        StartCoroutine(LoginRequest("username", "password"));
+    }
+ 
+    public void Login(){
+        Debug.Log("fields: " + usernameField.text + " " + passwordField.text);
+        StartCoroutine(LoginRequest(usernameField.text, passwordField.text));
+    }
+
+    IEnumerator GetText() {
+        UnityWebRequest www = UnityWebRequest.Get(leaderboard);
+        yield return www.SendWebRequest();
+ 
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+
+
+
+    IEnumerator LoginRequest(string username, string password) {
+        WWWForm form = new WWWForm();
+        form.AddField( "username", username);
+        form.AddField( "password", password);
+ 
+        UnityWebRequest www = UnityWebRequest.Post(login, form);
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+
+}
---
+++ Assets/Scripts/SceneLoaderScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+ 
+
+
+
+public class SceneLoaderScript : MonoBehaviour
+{
+    public void loadlevel(string level)
+    {
+        if(level.Equals("Register")){
+            SceneManager.LoadScene(1);
+        }
+        if(level.Equals("Login")){
+            SceneManager.LoadScene(2);
+        }
+    }
+}
